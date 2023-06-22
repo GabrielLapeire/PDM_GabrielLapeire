329436b75867fb7e9551c8e3a1a060d4276ed5a1
gabriel lapeire <lapeiregabriel@gmail.com>
2023-06-22 17:08:57 -0300

    22/6

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton = findViewById<Button>(R.id.btn_open_activity)
+        val miClick = MiOnClick()
+
+        boton.setOnClickListener(miClick)
+//        boton.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(this@MainActivity, "texto", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, MainActivity2::class.java)
+//            intent.putExtra("key", "value")
+//            startActivity(intent)
+//        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Main", "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Main", "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Main", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Main", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Main", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Main", "onDestroy")
+    }
+}
+
+class MiOnClick : View.OnClickListener {
+    override fun onClick(p0: View?) {
+        Toast.makeText(p0?.context, "texto", Toast.LENGTH_SHORT).show()
+        val intent = Intent(p0?.context, MainActivity2::class.java)
+        intent.putExtra("key", "value")
+        p0?.context?.startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: UserAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val value = intent.extras?.getString("key")
+        Log.i("some", value.toString())
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = UserAdapter(generarNombres())
+        recyclerView.adapter = adapter
+    }
+
+    private fun generarNombres(): List<String> {
+        val nameList = mutableListOf<String>()
+        for (i in 1..100) {
+            nameList.add("name $i")
+        }
+        return nameList
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainA2", "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainA2", "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainA2", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainA2", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainA2", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainA2", "onDestroy")
+    }
+}
+
+class UserAdapter(private val nameList: List<String>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+    class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+
+        fun bind(name:String) {
+            nameTextView.text = name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewTitle: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return nameList.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val name = nameList[position]
+        holder.bind(name)
+    }
+}
